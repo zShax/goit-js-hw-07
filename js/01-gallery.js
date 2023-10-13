778769b12a7becd6d8423a21639554c9b0dcc17d
zShax <71908965+zShax@users.noreply.github.com>
2023-10-12 21:37:29 -0700

    Finished HW 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.classList.add("gallery__item");
+
+  listItemEl.innerHTML = `<div class="gallery__item-content">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      data-source="${item.original}"
+    />
+  </div>`;
+
+  listEl.append(listItemEl);
+
+  const imageEl = listItemEl.querySelector(".gallery__image");
+  imageEl.addEventListener("click", () => {
+    openImageInLightbox(item.original, item.description);
+  });
+});
+
+function openImageInLightbox(imageSource, imageAlt) {
+  const instance = basicLightbox.create(`
+    <img src="${imageSource}" alt="${imageAlt}" />
+  `);
+
+  instance.show();
+
+  document.addEventListener("keydown", closeLightboxOnEsc);
+
+  function closeLightboxOnEsc(e) {
+    if (e.key === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", closeLightboxOnEsc);
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListElement = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const galleryItemElement = document.createElement("li");
+  galleryItemElement.classList.add("gallery__item");
+
+  galleryItemElement.innerHTML = `<a class="gallery__link" href="${item.original}">
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+   </a>`;
+
+  galleryListElement.append(galleryItemElement);
+});
+
+const imageLightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && imageLightbox.visible()) {
+    imageLightbox.close();
+  }
+});
+
+galleryListElement.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.tagName === "IMG") {
+    imageLightbox.open();
+  }
+});
